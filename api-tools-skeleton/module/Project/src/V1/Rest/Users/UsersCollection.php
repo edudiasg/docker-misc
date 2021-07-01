211903fbfed93db9f90a8c4f3af4c952e92bc33a
Eduardo Dias Gusmao <edg@outlook.com>
2021-06-30 22:43:37 -0300

    chore(org): folder org

--- api-tools-skeleton/module/Project/src/V1/Rest/Users/UsersCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Project\V1\Rest\Users;
-
-use Laminas\Paginator\Paginator;
-
-class UsersCollection extends Paginator
-{
-}
--- api-tools-skeleton/module/Project/src/V1/Rest/Users/UsersEntity.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Project\V1\Rest\Users;
-
-use ArrayObject;
-
-class UsersEntity extends ArrayObject
-{
-}
